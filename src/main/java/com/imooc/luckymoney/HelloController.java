6462ea450bd2e9c1457bdda4e4560936cffca3dc
weitao <weitao19950202@163.com>
2020-05-07 16:11:43 +0800

    first upload

---
+++ src/main/java/com/imooc/luckymoney/HelloController.java
@@ -0,0 +1,26 @@
+package com.imooc.luckymoney;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.math.BigDecimal;
+
+
+@Controller
+public class HelloController {
+
+    @Value("${minMoney}")
+    private BigDecimal minMoney;
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String say(){
+        return "hello wt: minMoney="+minMoney;
+    }
+
+
+
+
+
+}
